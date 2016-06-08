e4a6ad535d2f7769f42a06182205663fc62f15c5
Matthew Franglen <matthew@franglen.org>
2016-06-08 22:04:37 +0100

    Evaluate the input
    It's not applied yet, so it just says 0 whenever you do something more
    complicated than a constant

--- src/Evaluate.hs
+++ src/Evaluate.hs
@@ -6,11 +6,37 @@ module Evaluate
     )
     where
 
-data Expression = Constant Int
+import Text.Read (readMaybe)
+
+data Expression = CurrentValue
+                | Constant Int
                 | Add Expression Expression
                 | Subtract Expression Expression
                 | Multiply Expression Expression
                 | Divide Expression Expression
 
 evaluate :: String -> Expression
-evaluate _ = Constant 1
+evaluate = parse . tokenize
+
+data Token = TVerb String
+           | TConstant Int
+
+tokenize :: String -> [Token]
+tokenize xs = map f $ words xs
+    where f xs' = case readMaybe xs' :: Maybe Int of
+                    Just i  -> TConstant i
+                    Nothing -> TVerb xs'
+
+parse :: [Token] -> Expression
+parse ((TVerb v):xs)                 = parseVerb v CurrentValue $ parse xs
+parse ((TConstant x):(TVerb v):xs)   = parseVerb v (Constant x) $ parse xs
+parse ((TConstant x):[])             = Constant x
+parse []                             = CurrentValue
+parse _                              = error "Unknown expression"
+
+parseVerb :: String -> Expression -> Expression -> Expression
+parseVerb v x y | v == "add"      = Add x y
+                | v == "subtract" = Subtract x y
+                | v == "multiply" = Multiply x y
+                | v == "divide"   = Divide x y
+                | otherwise       = error $ "No such verb " ++ v
